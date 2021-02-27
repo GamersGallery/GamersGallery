dda3f024c3b09ed58e957e204b20cc97b492d973
Zachary Hardin <zacharyjhardin@yahoo.com>
2021-02-27 13:21:58 -0600

    JSON grabber implemented

--- ProjectMain/ProjectMain/Form1.Designer.cs
+++ ProjectMain/ProjectMain/Form1.Designer.cs
@@ -29,7 +29,26 @@ namespace ProjectMain
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGrabbingButton = new System.Windows.Forms.Button();
+            this.dataOutput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // dataGrabbingButton
+            // 
+            this.dataGrabbingButton.Location = new System.Drawing.Point(460, 243);
+            this.dataGrabbingButton.Name = "dataGrabbingButton";
+            this.dataGrabbingButton.Size = new System.Drawing.Size(75, 23);
+            this.dataGrabbingButton.TabIndex = 0;
+            this.dataGrabbingButton.Text = "Grab Data";
+            this.dataGrabbingButton.UseVisualStyleBackColor = true;
+            this.dataGrabbingButton.Click += new System.EventHandler(this.dataGrabbingButton_Click);
+            // 
+            // dataOutput
+            // 
+            this.dataOutput.Location = new System.Drawing.Point(447, 217);
+            this.dataOutput.Name = "dataOutput";
+            this.dataOutput.Size = new System.Drawing.Size(100, 20);
+            this.dataOutput.TabIndex = 1;
             // 
             // Form1
             // 
@@ -38,16 +57,22 @@ namespace ProjectMain
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1452, 647);
+            this.Controls.Add(this.dataOutput);
+            this.Controls.Add(this.dataGrabbingButton);
             this.HelpButton = true;
             this.Name = "Form1";
             this.Opacity = 0.6D;
             this.Text = "Gamer\'s Gallery";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button dataGrabbingButton;
+        private System.Windows.Forms.TextBox dataOutput;
     }
 }
 
--- ProjectMain/ProjectMain/Form1.cs
+++ ProjectMain/ProjectMain/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
 
 namespace ProjectMain
 {
@@ -22,9 +25,17 @@ namespace ProjectMain
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void dataGrabbingButton_Click(object sender, EventArgs e)
         {
-
+            string url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?&include_appinfo&key=415D9AE08A76A31839DC375BFB2E1975&steamid=76561198071247386&include_appinfo=true";
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            string jsonValue = "";
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)  
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                jsonValue = reader.ReadToEnd();
+            }
+            dataOutput.Text = jsonValue;
         }
     }
 }
