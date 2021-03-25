09d2773e40b75b742d3e3810197903289dc2f1dc
Sutton <33501521+PBHotdog@users.noreply.github.com>
2021-03-25 12:22:58 -0500

    Temp changes to Site.Master

--- GamerGallery/GamerGallery/Site.Master.cs
+++ GamerGallery/GamerGallery/Site.Master.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Web.Security;
+using Microsoft.Owin.Security.Cookies;
 
 namespace GamerGallery
 {
@@ -12,6 +16,19 @@ namespace GamerGallery
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.Current.GetOwinContext().Authentication.Challenge(
+                  new AuthenticationProperties { RedirectUri = "/" },
+                  OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+        protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+        {
+            Context.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
         }
     }
 }
