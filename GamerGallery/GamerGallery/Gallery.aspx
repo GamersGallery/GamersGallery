43275d97e3b9b34f229d523a8b994937a33171cf
Zachary Hardin <zacharyjhardin@yahoo.com>
2021-03-31 13:01:08 -0500

    JSON is now more restrictive.

--- GamerGallery/GamerGallery/Gallery.aspx
+++ GamerGallery/GamerGallery/Gallery.aspx
@@ -1,4 +1,4 @@
-﻿    <%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Gallery.aspx.cs" Inherits="GamerGallery.Contact" %>
+﻿    <%@ Page Title="Your Gallery" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Gallery.aspx.cs" Inherits="GamerGallery.Contact" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
         <div runat="server" id="divGalleryGrid">
--- GamerGallery/GamerGallery/Gallery.aspx.cs
+++ GamerGallery/GamerGallery/Gallery.aspx.cs
@@ -17,7 +17,7 @@ namespace GamerGallery
         {
             if (!IsPostBack) //code will eventually live here
             {
-                
+
             }
         }
         protected void Fill_Gallery(long steamID)
@@ -30,6 +30,14 @@ namespace GamerGallery
                 StreamReader reader = new StreamReader(response.GetResponseStream());
                 jsonString = reader.ReadToEnd();
             }
+
+            jsonString = parseJSON(jsonString);
+            DataTable gallery = jsonConversion(jsonString);
+            galleryGrid.DataSource = gallery;
+            galleryGrid.DataBind();
+        }
+        public string parseJSON(string jsonString)
+        {
             string s = string.Empty;
             int gamesNum = 0;
             for (int i = 0; i < 30; i++)
@@ -39,18 +47,66 @@ namespace GamerGallery
                     s += jsonString[i];
                 }
             }
-
             if (s.Length > 0)
             {
                 gamesNum = int.Parse(s);
             }
-
-            testTextbox.Text = "You own " + gamesNum.ToString() + " games on Steam";
-
-
-            DataTable gallery = jsonConversion(jsonString);
-            galleryGrid.DataSource = gallery;
-            galleryGrid.DataBind();
+            testTextbox.Text = "You own " + gamesNum.ToString() + " games on Steam"; //test code | will delete after integration is complete
+            jsonString = jsonString.Replace("{\"response\":{\"game_count\":" + gamesNum.ToString() + ",\"games\":[", string.Empty);
+            s = string.Empty;
+            int gameNum = 0;
+            for (int i = 0; i < jsonString.Length; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    if (i + j >= jsonString.Length)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        s += jsonString[i + j];
+                        if (s == ",\"img_icon_url\":")
+                        {
+                            s = string.Empty;
+                            for (int k = 0; k < 200; k++)
+                            {
+                                for (int l = 0; l < 25; l++)
+                                {
+                                    if (i + j + k + l >= jsonString.Length)
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        s += jsonString[i + j + k + l];
+                                        if (s == "\"playtime_linux_forever\":")
+                                        {
+                                            s = string.Empty;
+                                            int linuxDigit = 1;
+                                            for (int m = 0; m < 7; m++)
+                                            {
+                                                if (i + j + k + l + m >= jsonString.Length)
+                                                {
+                                                    break;
+                                                }
+                                                else if (Char.IsDigit(jsonString[i+j+k+l+m]))
+                                                {
+                                                    linuxDigit++;
+                                                }
+                                            }
+                                            jsonString = jsonString.Remove(i, j + k + l + linuxDigit);
+                                        }
+                                    }
+                                }
+                                s = string.Empty;
+                            }
+                        }
+                    }
+                }
+                s = string.Empty;
+            }
+            return jsonString;
         }
         public DataTable jsonConversion(string jsonString)
         {
@@ -74,7 +130,6 @@ namespace GamerGallery
                     catch (Exception ex)
                     {
                         throw new Exception(string.Format("Error Parsing Column Name : {0}", columnName));
-
                     }
                 }
                 break;
