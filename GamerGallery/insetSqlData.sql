c0c38ffda0b5587063a6a095ff0b715374e370ee
Sutton <33501521+PBHotdog@users.noreply.github.com>
2021-04-08 15:40:20 -0500

    Added Database/Sql Data

---
+++ GamerGallery/insetSqlData.sql
@@ -0,0 +1,6 @@
+﻿create database userInformation
+create table userLogin( UserID varchar(50) primary key not null, UserPass varchar(100) not null, SteamID varchar(200) not null)
+insert into userLogin values('Zach','Minecraft','76561198071247386')
+insert into userLogin values('Sutton','Pogchamp','76561198027554025')
+insert into userLogin values('Ryan','Sword','76561198016757877')
+insert into userLogin values('Collin','Smite','76561198037199400')
